dcbf2f0d249e0573f3a99d71f80768f58967f1de
Justin Carlson <jcarlson1985420@gmail.com>
2016-10-05 10:36:09 -0500

    Add CORS support for remote calls

--- app.js
+++ app.js
@@ -40,6 +40,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
+// Enable CORS in the response headers
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 // define routing for Express
 var home = require('./routes/home');
 var admin = require('./routes/admin');
