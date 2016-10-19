3ae05a6b2043bac467e759d826bfd5aa1f0fb223
Justin Carlson <jcarlson1985420@gmail.com>
2016-10-19 09:59:58 -0500

    Added utility script to be able to create a new user from the command line

---
+++ createuser.js
@@ -0,0 +1,31 @@
+// how to call this script:
+// node createuser.js username password, such as "node createuser.js admin p@ssword1"
+
+//initialize config options
+var configOptions = require('./config/config.js');
+
+// Mongoose
+var mongoose = require('mongoose');
+require('./models/Users.js');
+var User = mongoose.model('GarageStatus');
+
+// Connect to MongoDB
+var mongoConnectionString = configOptions.MONGO_DB_CONNECTION_STRING;
+mongoose.connect(mongoConnectionString);
+
+// get the arguments
+var userName = process.argv[2];
+var password = process.argv[3];
+
+var newUser = new User();
+newUser.username = userName.toLowerCase();
+newUser.setPassword(password);
+
+newUser.save(function(err, insertedUser) {
+    if (err) {
+        console.log('Unable to insert user: ' + err);
+    }
+    else {
+        console.log('Successfully inserted user: ' + userName.toLowerCase() + '!');
+    }
+});
